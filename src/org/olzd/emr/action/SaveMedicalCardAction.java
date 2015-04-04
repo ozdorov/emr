231c8b861375fcd99427e486148db0ddf49915a1
ozdorov <oleksandr.zdorov@sjrb.ca>
2015-04-04 21:50:24 +0300

    Before saving of new card, check if there are no with similar name and surname

--- src/org/olzd/emr/action/SaveMedicalCardAction.java
+++ src/org/olzd/emr/action/SaveMedicalCardAction.java
@@ -1,12 +1,14 @@
 package org.olzd.emr.action;
 
 import org.olzd.emr.entity.MedicalCard;
+import org.olzd.emr.model.SearchResult;
 import org.olzd.emr.service.MedicalCardService;
 import org.olzd.emr.view.EditMedicalCardPanel;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.ActionEvent;
+import java.util.List;
 
 public class SaveMedicalCardAction extends AbstractAction {
     private EditMedicalCardPanel panel;
@@ -23,6 +25,11 @@ public class SaveMedicalCardAction extends AbstractAction {
         if (card.isExisting()) {
             cardService.updateMedicalCard(card);
         } else {
+            List<SearchResult> existing = cardService.findExistingMedicalCard(card.getSurname(), card.getName());
+            if (!existing.isEmpty()) {
+                JOptionPane.showMessageDialog(panel.getParentFrame(), "Карточка с указанными именем и фамилией уже существует");
+                return;
+            }
             cardService.createMedicalCard(card);
         }
 
